83d338f83e7f37f35b176a291102f9a645ee2d5d
walerian777 <walerian.sobczak@gmail.com>
2016-12-22 16:22:02 +0100

    Created users table

---
+++ db/migrate/20161222141208_add_uuid_extension.rb
@@ -0,0 +1,13 @@
+class AddUuidExtension < ActiveRecord::Migration[5.0]
+  def up
+    execute <<-SQL
+      CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP EXTENSION 'uuid-ossp';
+    SQL
+  end
+end
