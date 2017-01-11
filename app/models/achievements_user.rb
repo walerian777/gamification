9491a1f541f0b984ddb72e0fb99b81960da58826
walerian777 <walerian.sobczak@gmail.com>
2017-01-12 00:21:34 +0100

    Added assigning experience to a user after getting an achievement

--- app/models/achievements_user.rb
+++ app/models/achievements_user.rb
@@ -3,4 +3,12 @@ class AchievementsUser < ApplicationRecord
   belongs_to :user
 
   validates :achievement_id, :user_id, presence: true
+
+  after_create :assing_experience
+
+  private
+
+  def assing_experience
+    user.update_attribute(:experience, user.experience + achievement.points)
+  end
 end
