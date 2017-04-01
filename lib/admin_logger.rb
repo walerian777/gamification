6df11666989c9b410a7b9ed5abb015551bcf2150
walerian777 <walerian.sobczak@gmail.com>
2017-04-01 20:35:51 +0200

    Revert "Added AdminLogger (singleton)"
    This reverts commit ebc9d2e271cdae3cb7d6e5eb293591b6860539c9.

--- lib/admin_logger.rb
+++
@@ -1,7 +0,0 @@
-module AdminLogger
-  def self.log(message)
-    @@log ||= File.open('./log/admin_access.log', 'a')
-    @@log.write("#{DateTime.current}: #{message}\n")
-    @@log.close
-  end
-end
