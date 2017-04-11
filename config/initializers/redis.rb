6c9685f4f3c062e92c6e4011c5c052f497224bfc
walerian777 <walerian.sobczak@gmail.com>
2017-04-11 19:34:28 +0200

    Fixed redis initializer #2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,10 @@
 require 'redis'
 require 'redis/objects'
 
-REDIS_CONFIG = YAML.load(File.open(Rails.root.join('config/redis.yml')))
-config = REDIS_CONFIG[Rails.env].symbolize_keys
+if Rails.env.test? || Rails.env.development? || ENV['REDIS_URL'].present?
+  REDIS_CONFIG = YAML.load(File.open(Rails.root.join('config/redis.yml')))
+  config = REDIS_CONFIG[Rails.env].symbolize_keys
 
-if !Rails.env.production? || ENV['REDIS_URL'].present?
   redis = Redis.new(config)
   Redis::Objects.redis = redis
 end
