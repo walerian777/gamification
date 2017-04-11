932627f458971292361335013ca2f3a2d3e0221a
walerian777 <walerian.sobczak@gmail.com>
2017-04-11 17:49:16 +0200

    Fixed uuid extension migration

--- db/migrate/20161222141208_add_uuid_extension.rb
+++ db/migrate/20161222141208_add_uuid_extension.rb
@@ -1,7 +1,7 @@
 class AddUuidExtension < ActiveRecord::Migration[5.0]
   def up
     execute <<-SQL
-      CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
+      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     SQL
   end
 
